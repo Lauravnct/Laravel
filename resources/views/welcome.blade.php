2cd9ce8005f1fa89ba9c2937b3234925fb9053b7
lisaadilia <lisa.da_cunha@devinci.fr>
2017-02-10 12:09:48 +0100

    Changement du title de la home + gras

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Rendus Lisa Da Cunha & Laura Vincent
+                    <strong>Rendus Lisa Da Cunha & Laura Vincent</strong>
                 </div>
 
                 <div class="links">
